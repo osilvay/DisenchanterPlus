fee74944b84fcf8ec2f0f6ed7e19490ae52092e8
Oscar Silva <osilvay@gmail.com>
2025-01-10 10:50:54 +0100

    Cambio de version

--- DisenchanterPlus.lua
+++ DisenchanterPlus.lua
@@ -21,7 +21,7 @@ DisenchanterPlus.DEBUG_INFO = "|cff00bc32[INFO]|r"
 DisenchanterPlus.DEBUG_DEVELOP = "|cff7c83ff[DEVELOP]|r"
 DisenchanterPlus.DEBUG_SPAM = "|cffff8484[SPAM]|r"
 local AddonColor = "ffed6bff"
-local AddonVersion = "1.0.9.0"
+local AddonVersion = "1.0.9.1"
 
 BINDING_NAME_DISENCHANTER_PLUS_ACCEPT_DISENCHANT = L["Accept disenchant"]
 BINDING_NAME_DISENCHANTER_PLUS_CANCEL_DISENCHANT = L["Cancel disenchant"]
