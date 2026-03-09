d4c34b61b63c3e16fa0a702748fbfb58f7428a8b
rcouch00 <rcouch00@gmail.com>
2026-03-09 14:14:54 -0500

    Hide Minimap icon
    Adds Minimap toggle visibility general option as well as slash command

--- Modules/Settings/GeneralTab/Groups/GeneralGroup.lua
+++ Modules/Settings/GeneralTab/Groups/GeneralGroup.lua
@@ -24,6 +24,9 @@ end
 ---@param order? number
 ---@return table
 function DP_GeneralGroup:Config(order)
+  -- Import the Minimap module so we can call the toggle function
+  local DP_MinimapIcon = DP_ModuleLoader:ImportModule("DP_MinimapIcon")
+
   return {
     type = "group",
     order = order,
@@ -43,6 +46,24 @@ function DP_GeneralGroup:Config(order)
         set = function(info, value)
           DP_GeneralGroup:CheckStatus(value)
           DisenchanterPlus.db.char.general.disableAll = value
+        end,
+      },
+      -- NEW: Hide Minimap Icon Toggle
+      hideMinimap = {
+        type = "toggle",
+        order = 2, -- Appears below "Disable all"
+        name = "Hide Minimap Icon", -- You can wrap this in DP_i18n if you have translations
+        desc = "Hide the DisenchanterPlus button from the minimap.",
+        width = "full",
+        get = function()
+          return DisenchanterPlus.db.profile.minimap.hide
+        end,
+        set = function(info, value)
+          DisenchanterPlus.db.profile.minimap.hide = value
+          -- Call the visibility function to apply the change immediately
+          if DP_MinimapIcon and DP_MinimapIcon.ToggleVisibility then
+            DP_MinimapIcon:ToggleVisibility()
+          end
         end,
       },
     },
